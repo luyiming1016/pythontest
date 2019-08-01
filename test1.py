7a29bf7f5fa49e3dbb0350088df46bae245da1d6
Arthur <luyiming1016@gmail.com>
2019-08-01 11:33:22 +0800

    first commit

---
+++ test1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+str=input('please input your name:')
+print(str)
+print(str[0:-1])
+print(str[0])
+print(str[2:5])
+print(str[2:])
+print(str*2)
+print(str+'你好')
+
+print('----------')
+
+print('hello\nrunoob')
+print(r'hello\nrunoob')
